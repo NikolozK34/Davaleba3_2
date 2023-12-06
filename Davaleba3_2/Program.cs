9a65adc6335ba2762d091ea84cc504c0c59ca4f2
NikolozK34 <kekelidze97@gmail.com>
2023-12-06 15:47:08 +0400

    Add project files.

---
+++ Davaleba3_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Davaleba3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sheuyvane ricxvi: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(n * i);
+            }
+        }
+    }
+}
